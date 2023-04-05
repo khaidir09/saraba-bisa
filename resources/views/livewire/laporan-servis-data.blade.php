55ef36a0d8541ebf8beb3baf84ba21e054733777
Muhammad Khaidir <khaedir3018@gmail.com>
2023-04-05 22:28:33 +0800

    update merapikan header akun teknisi

--- resources/views/livewire/laporan-servis-data.blade.php
+++ resources/views/livewire/laporan-servis-data.blade.php
@@ -7,8 +7,13 @@
             <option value="100">100</option>
         </select>
 
-        <!-- Search form -->
-        <x-search-form placeholder="YYYY-MM-DD" />
+        <!-- Right: Actions -->
+        <div class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
+
+            <!-- Search form -->
+            <x-search-form placeholder="YYYY-MM-DD" />
+
+        </div>
     </div>
 
     <div class="bg-white shadow-lg rounded-sm border border-slate-200 mt-5 mb-8">
