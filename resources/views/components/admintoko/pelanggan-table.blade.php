e02525db5b27705202c28597a78752f84a7e0783
Muhammad Khaidir <khaedir3018@gmail.com>
2023-04-26 07:56:14 +0800

    hapus file tidak terpakai

--- resources/views/components/admintoko/pelanggan-table.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="bg-white shadow-lg rounded-sm border border-slate-200 mb-8">
-    <header class="px-5 py-4">
-        <h2 class="font-semibold text-slate-800">Semua Pelanggan <span class="text-slate-400 font-medium">{{ $count }}</span></h2>
-    </header>
-    <!-- Table -->
-    <div class="overflow-x-auto">
-        <livewire:customer-data></livewire:customer-data>
-    </div>
-</div>
