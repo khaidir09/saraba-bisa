94050a0482dc33aa041f89b07c33c1ce8a52dda9
Muhammad Khaidir <khaedir3018@gmail.com>
2023-05-09 12:11:02 +0800

    reset welcome banner

--- resources/views/pages/kepalatoko/dashboard.blade.php
+++ resources/views/pages/kepalatoko/dashboard.blade.php
@@ -6,7 +6,7 @@
     <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
         
         <!-- Welcome banner -->
-        <x-dashboard.welcome-banner :totalomzet="$totalomzet" :totalprofitutuh="$totalprofitutuh"/>
+        <x-dashboard.welcome-banner/>
 
         <!-- Banner -->
         <div class="mb-6">
