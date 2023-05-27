1364bf395d9b5ae780f3b240b6bf9ca73bab6479
Muhammad Khaidir <khaedir3018@gmail.com>
2023-05-27 16:46:45 +0800

    mengubah inputan kerusakan menjadi manual

--- resources/views/livewire/teknisi-bisa-diambil-data.blade.php
+++ resources/views/livewire/teknisi-bisa-diambil-data.blade.php
@@ -117,8 +117,8 @@
                                         <input id="kelengkapan" name="kelengkapan" class="form-input w-full px-2 py-1" type="text" placeholder="Kosongkan jika kelengkapannya hanya unit"/>
                                     </div>
                                     <div>
-                                        <label class="block text-sm font-medium mb-1" for="kerusakan">Kerusakan <span class="text-rose-500">*</span> </label>
-                                        <livewire:kerusakan-search></livewire:kerusakan-search>
+                                        <label class="block text-sm font-medium mb-1" for="kerusakan">Kerusakan <span class="text-rose-500">*</span></label>
+                                        <input id="kerusakan" name="kerusakan" class="form-input w-full px-2 py-1" type="text" required/>
                                     </div>
                                     <div>
                                         <label class="block text-sm font-medium mb-1" for="qc_masuk">Pengecekan Fungsi <span class="text-rose-500">*</span></label>
