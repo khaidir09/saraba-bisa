6e76f3c32bc1ee2cdb3d7fccb5bdb0980417e0d4
Muhammad Khaidir <khaedir3018@gmail.com>
2023-06-01 13:31:36 +0800

    menambahkan filter teknisi pada laporan servis

--- resources/views/livewire/laporan-servis-data.blade.php
+++ resources/views/livewire/laporan-servis-data.blade.php
@@ -10,6 +10,46 @@
         <!-- Right: Actions -->
         <div class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
 
+            <!-- Filter button -->
+            <div class="relative inline-flex hidden md:block" x-data="{ open: false }">
+                <button
+                    class="btn bg-white border-slate-200 hover:border-slate-300 text-slate-500 hover:text-slate-600"
+                    aria-haspopup="true"
+                    @click.prevent="open = !open"
+                    :aria-expanded="open"
+                >
+                    <span class="sr-only">Filter</span><wbr>
+                    <svg class="w-4 h-4 fill-current" viewBox="0 0 16 16">
+                        <path d="M9 15H7a1 1 0 010-2h2a1 1 0 010 2zM11 11H5a1 1 0 010-2h6a1 1 0 010 2zM13 7H3a1 1 0 010-2h10a1 1 0 010 2zM15 3H1a1 1 0 010-2h14a1 1 0 010 2z" />
+                    </svg>
+                </button>
+                <div
+                    class="origin-top-right z-10 absolute top-full min-w-56 bg-white border border-slate-200 pt-1.5 rounded shadow-lg overflow-hidden mt-1 right-4"                
+                    @click.outside="open = false"
+                    @keydown.escape.window="open = false"
+                    x-show="open"
+                    x-transition:enter="transition ease-out duration-200 transform"
+                    x-transition:enter-start="opacity-0 -translate-y-2"
+                    x-transition:enter-end="opacity-100 translate-y-0"
+                    x-transition:leave="transition ease-out duration-200"
+                    x-transition:leave-start="opacity-100"
+                    x-transition:leave-end="opacity-0"
+                    x-cloak                
+                >
+                    <div class="text-xs font-semibold text-slate-400 uppercase pt-1.5 pb-2 px-4">Filter</div>
+                    <ul class="mb-4">
+                        @foreach ($users as $item)
+                            <li class="py-1 px-3">
+                                <label class="flex items-center">
+                                    <input type="checkbox" class="form-checkbox" wire:model="user" value="{{ $item->id }}"/>
+                                    <span class="text-sm font-medium ml-2">{{ $item->name }}</span>
+                                </label>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+
             <!-- Search form -->
             <x-search-form placeholder="YYYY-MM-DD" />
 
