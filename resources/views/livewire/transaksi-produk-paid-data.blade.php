b60508b1781415a4d4b2fb8cbc77a6c345850b13
Muhammad Khaidir <khaedir3018@gmail.com>
2023-06-13 15:09:32 +0800

    remove edit button

--- resources/views/livewire/transaksi-produk-paid-data.blade.php
+++ resources/views/livewire/transaksi-produk-paid-data.blade.php
@@ -102,14 +102,7 @@
                                 <div class="font-medium">{{ $i++ }}</div>
                             </td>
                             <td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                                <a href="#">
-                                    <div class="flex items-center text-blue-600">
-                                        <svg class="w-6 h-6 fill-current" viewBox="0 0 32 32">
-                                            <path d="M19.7 8.3c-.4-.4-1-.4-1.4 0l-10 10c-.2.2-.3.4-.3.7v4c0 .6.4 1 1 1h4c.3 0 .5-.1.7-.3l10-10c.4-.4.4-1 0-1.4l-4-4zM12.6 22H10v-2.6l6-6 2.6 2.6-6 6zm7.4-7.4L17.4 12l1.6-1.6 2.6 2.6-1.6 1.6z" />
-                                        </svg>
-                                        <div class="font-medium">{{ $item->invoice_no }}</div>
-                                    </div>
-                                </a>
+                                <div class="font-medium text-blue-600">#{{ $item->invoice_no }}</div>
                             </td>
                             <td class="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
                                 <div>{{ \Carbon\Carbon::parse($item->order_date)->format('d/m/Y') }}</div>
