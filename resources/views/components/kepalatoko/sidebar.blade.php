efd56082afae1369eb820a12bd722a492c08f475
Muhammad Khaidir <khaedir3018@gmail.com>
2023-06-27 10:05:54 +0800

    menyembunyikan laporan admin dan bonus admin

--- resources/views/components/kepalatoko/sidebar.blade.php
+++ resources/views/components/kepalatoko/sidebar.blade.php
@@ -256,11 +256,11 @@
                                         <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Laporan Sales</span>
                                     </a>
                                 </li>
-                                <li class="mb-1 last:mb-0">
+                                {{-- <li class="mb-1 last:mb-0">
                                     <a class="block text-slate-400 hover:text-slate-200 transition duration-150 truncate @if(Route::is('laporan-admin')){{ '!text-indigo-500' }}@endif" href="{{ route('laporan-admin') }}">
                                         <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Laporan Admin</span>
                                     </a>
-                                </li>
+                                </li> --}}
                             </ul>
                         </div>
                     </li>
