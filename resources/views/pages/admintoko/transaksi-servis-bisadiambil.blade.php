2dd67e3c22f5b7bd66d4c7e1b1af225f71c8f0be
Muhammad Khaidir <khaedir3018@gmail.com>
2023-07-06 20:59:43 +0800

    menambahkan kondisi apabila akun teknisi sudah dihapus saat mengubah status bisa diambil

--- resources/views/pages/admintoko/transaksi-servis-bisadiambil.blade.php
+++ resources/views/pages/admintoko/transaksi-servis-bisadiambil.blade.php
@@ -107,6 +107,11 @@
                                 <div>
                                     <label class="block text-sm font-medium mb-1" for="users_id">Teknisi</label>
                                     <select id="users_id" name="users_id" class="form-select text-sm py-1 w-full" required>
+                                        @if ($item->user != null)
+                                            <option selected value="{{ $item->user->id }}">{{ $item->user->name }}</option>
+                                        @else
+                                            <option selected value="">Data akun telah dihapus</option>
+                                        @endif
                                         @foreach ($users as $user)
                                             <option value="{{ $user->id }}">{{ $user->name }}</option>
                                         @endforeach
