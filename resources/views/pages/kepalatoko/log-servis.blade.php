0a4d1a401ac3289e865e832c0dd849f2c3ca0a41
Muhammad Khaidir <khaedir3018@gmail.com>
2023-08-29 16:14:03 +0800

    ganti icon bersihkan semua riwayat

--- resources/views/pages/kepalatoko/log-servis.blade.php
+++ resources/views/pages/kepalatoko/log-servis.blade.php
@@ -19,8 +19,13 @@
                 <!-- Start Delete-->
                 <div x-data="{ modalOpen: false }">
                     <button class="btn bg-rose-500 hover:bg-rose-600 text-white" @click.prevent="modalOpen = true" aria-controls="tambah-modal">
-                        <svg class="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
-                            <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M4 7l16 0" />
+                        <path d="M10 11l0 6" />
+                        <path d="M14 11l0 6" />
+                        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
                         </svg>
                         <span class="hidden xs:block ml-2">Bersihkan Semua Riwayat</span>
                     </button>
