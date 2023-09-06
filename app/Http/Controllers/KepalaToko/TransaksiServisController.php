3c9540f9004e675dbd0217a838bb9316e18d682e
Muhammad Khaidir <khaedir3018@gmail.com>
2023-09-06 10:39:28 +0800

    Update TransaksiServisController.php

--- app/Http/Controllers/KepalaToko/TransaksiServisController.php
+++ app/Http/Controllers/KepalaToko/TransaksiServisController.php
@@ -63,7 +63,6 @@ class TransaksiServisController extends Controller
     {
         $nomor_servis = '' . mt_rand(date('Ymd00'), date('Ymd99'));
         $nama_pelanggan = Customer::find($request->customers_id);
-        $nama_tindakan = ServiceAction::find($request->kerusakan);
 
         // Transaction create
         ServiceTransaction::create([
@@ -77,7 +76,7 @@ class TransaksiServisController extends Controller
             'warna' => $request->warna,
             'capacities_id' => $request->capacities_id,
             'kelengkapan' => $request->kelengkapan,
-            'kerusakan' => $nama_tindakan->nama_tindakan,
+            'kerusakan' => $request->kerusakan,
             'qc_masuk' => $request->qc_masuk,
             'estimasi_pengerjaan' => $request->estimasi_pengerjaan,
             'estimasi_biaya' => $request->estimasi_biaya,
