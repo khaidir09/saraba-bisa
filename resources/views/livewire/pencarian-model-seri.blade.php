c988fb316b52b66cf10ff8560336849fdf45fcec
Muhammad Khaidir <khaedir3018@gmail.com>
2023-10-23 23:22:09 +0800

    delete master version

--- resources/views/livewire/pencarian-model-seri.blade.php
+++
@@ -1,9 +0,0 @@
-<div>
-        <x-select
-            wire:model.defer="model_series_id"
-            placeholder="Pilih Model Seri"
-            :options="$model_series"
-            option-label="name"
-            option-value="id"
-        />
-</div>
