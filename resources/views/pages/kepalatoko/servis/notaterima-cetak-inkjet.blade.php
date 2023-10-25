3bd6878a03c9ed18564af068ac0f977047a5141c
Muhammad Khaidir <khaedir3018@gmail.com>
2023-10-25 08:24:26 +0800

    ganti font dot matrix

--- resources/views/pages/kepalatoko/servis/notaterima-cetak-inkjet.blade.php
+++ resources/views/pages/kepalatoko/servis/notaterima-cetak-inkjet.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Tanda Terima Servis #{{ $items->nomor_servis }}</title>
+	<link href="https://fonts.cdnfonts.com/css/dot-matrix" rel="stylesheet">
 	<style>
 		@page {
             size: A4; /* Ukuran halaman A4, bisa disesuaikan */
@@ -12,6 +13,7 @@
         }
 		body {
 			margin: 0;
+			font-family: 'Dot Matrix', sans-serif;
 		}
 
 		.text-center {
