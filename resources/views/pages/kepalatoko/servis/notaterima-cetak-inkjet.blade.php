cfd69706dbe0d188a9943d4ab686616c0a1e14f8
Muhammad Khaidir <khaedir3018@gmail.com>
2023-10-25 08:41:52 +0800

    update font

--- resources/views/pages/kepalatoko/servis/notaterima-cetak-inkjet.blade.php
+++ resources/views/pages/kepalatoko/servis/notaterima-cetak-inkjet.blade.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Tanda Terima Servis #{{ $items->nomor_servis }}</title>
-	<link href="https://fonts.cdnfonts.com/css/dot-matrix" rel="stylesheet">
 	<style>
 		@page {
             size: A4; /* Ukuran halaman A4, bisa disesuaikan */
