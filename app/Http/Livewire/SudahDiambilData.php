c91026fa924ac95902156059d4621996452c62be
Muhammad Khaidir <khaedir3018@gmail.com>
2023-10-26 15:12:14 +0800

    mengubah urutan data sudah diambil berdasarkan tgl ambil

--- app/Http/Livewire/SudahDiambilData.php
+++ app/Http/Livewire/SudahDiambilData.php
@@ -74,8 +74,8 @@ class SudahDiambilData extends Component
             'jumlah_bisa_diambil' => $jumlah_bisa_diambil,
             'jumlah_sudah_diambil' => $jumlah_sudah_diambil,
             'service_transactions' => $this->search === null ?
-                ServiceTransaction::latest()->where('status_servis', 'Sudah Diambil')->whereIn('types_id', $this->type)->whereIn('kondisi_servis', $this->kondisi)->paginate($this->paginate) :
-                ServiceTransaction::latest()->where('status_servis', 'Sudah Diambil')->where('nama_pelanggan', 'like', '%' . $this->search . '%')->orWhere('nomor_servis', $this->search)->paginate($this->paginate)
+                ServiceTransaction::orderBy('tgl_ambil', 'desc')->where('status_servis', 'Sudah Diambil')->whereIn('types_id', $this->type)->whereIn('kondisi_servis', $this->kondisi)->paginate($this->paginate) :
+                ServiceTransaction::orderBy('tgl_ambil', 'desc')->where('status_servis', 'Sudah Diambil')->where('nama_pelanggan', 'like', '%' . $this->search . '%')->orWhere('nomor_servis', $this->search)->paginate($this->paginate)
         ]);
     }
 }
